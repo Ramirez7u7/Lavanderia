d5bb17e02475621b6074beac44be53c527bd3495
Ramirez <utm23090438@utma.edu.mx.com>
2025-06-19 11:14:21 -0600

    order update

--- Server/app/controllers/order_controller.py
+++ Server/app/controllers/order_controller.py
@@ -32,3 +32,27 @@ def create_order_detail(garment_id, service_id, quantity):
     db.session.add(order_detail)
     db.session.commit()
     return order_detail
+
+
+
+
+
+def update_order_total(order_id, new_status):
+    order = Order.query.get(order_id)
+    if not order:
+        return None
+    order.state = new_status
+    db.session.commit()
+    return order
+
+def list_orders_by_status(status):
+    orders = Order.query.filter_by(state=status).all()
+    data = [{
+        "id": o.id,
+        "client_id": o.client_id,
+        "state": o.state,
+        "estimated_delivery_date": o.estimated_delivery_date,
+        "total": o.total,
+        "pagago": o.pagado,
+    } for o in orders]
+    return data
